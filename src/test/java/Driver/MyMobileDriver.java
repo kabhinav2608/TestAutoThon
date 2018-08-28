09edc9faba56be3fff7fc108d176526a1fad4347
Kumar Abhinav <kumar.abhinav@gdn-commerce.com>
2018-08-28 19:59:59 +0530

    Added model and used mobile driver

--- src/test/java/Driver/MyMobileDriver.java
+++ src/test/java/Driver/MyMobileDriver.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -23,11 +24,24 @@ public class MyMobileDriver{
     appiumService.start();
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0.0");
     capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "4000");
     capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
-    capabilities.setCapability("chromedriverExecutable","TestathonProj/chromedriver");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"mi a1");
+    capabilities.setCapability("chromedriverExecutable","chromedriver");
     return new AndroidDriver(new URL(appiumService.getUrl().toString()), capabilities);
 
+  }
+
+  public static void main(String[] args) {
+    MyMobileDriver myMobileDriver = new MyMobileDriver();
+    try {
+      WebDriver driver =  myMobileDriver.mobileDriver();
+      driver.get("https://www.google.com");
+      System.out.println("-----Current URL---"+driver.getCurrentUrl());
+      driver.quit();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
   }
 }
--- src/test/java/TestathonProj/steps/MultiPlatformSteps.java
+++ src/test/java/TestathonProj/steps/MultiPlatformSteps.java
@@ -1,11 +1,16 @@
 package TestathonProj.steps;
 
+import Driver.MyMobileDriver;
+import TestathonProj.Model.TestModel;
 import TestathonProj.api.TestApi;
 import TestathonProj.steps.serenity.EndUserSteps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
+import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
 
 /**
  * @author kumar on 28/08/18
@@ -18,6 +23,8 @@ public class MultiPlatformSteps {
   EndUserSteps endUserSteps;
 
   TestApi testApi = new TestApi();
+  MyMobileDriver myMobileDriver = new MyMobileDriver();
+  TestModel testModel = new TestModel();
 
   String title ;
 
@@ -36,6 +43,7 @@ public class MultiPlatformSteps {
     System.out.println("----title--"+title);
     if(title!=null|| !title.isEmpty()){
       String response = testApi.prepareRequest();
+      testModel.setApiResponse(response);
       System.out.println("-----response----"+response);
     }
   }
@@ -43,5 +51,15 @@ public class MultiPlatformSteps {
   @Then("^data is passed to different env$")
   public void dataIsPassedToDifferentEnv(){
 
+    try {
+    if(testModel.getApiResponse()!=null || testModel.getApiResponse().isEmpty()){
+      WebDriver driver = myMobileDriver.mobileDriver();
+      driver.get("https://www.google.com");
+      System.out.println("-----Current URL---"+driver.getCurrentUrl());
+      driver.quit();
+    }
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
   }
 }
